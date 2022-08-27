c15a802f651c4ae361084bce0eedb11db7f48717
keweizhou <1514991931@qq.com>
2022-08-27 17:06:04 +0800

    多个配置文件

--- springboot/src/main/java/com/example/springboot/Application.java
+++ springboot/src/main/java/com/example/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+    //java –jar xx.jar --spring.profiles.active=dev
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
